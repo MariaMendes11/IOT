837f04edd099f3ad0fe0af9f467c6828181ffcab
MariaMendes11 <mcmendes900@gmail.com>
2025-09-08 12:41:53 -0300

    buzzer

--- botaoled/botaoled.ino
+++ botaoled/botaoled.ino
@@ -1,4 +1,17 @@
+
 #include "AdafruitIO_WiFi.h"
+#include "NewPing.h"
+
+#define BUZZER_PIN 27
+#define LED_ALARME 13
+#define BOTAO_FISICO 26
+#define TRIG_PIN 12
+#define ECHO_PIN 14
+
+
+//Configuraçãodo ultrassonico
+#define MAX_DISTENCE 100
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTENCE);
 
 // suas credenciais Wi-Fi
 #define WIFI_SSID       ""
@@ -27,12 +40,23 @@ void setup() {
   // Configura o pino do LED como saída
   pinMode(LED_PIN, OUTPUT);
 
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_ALARME, OUTPUT);
+  pinMode(BOTAO_FISICO, INPUT);
+
+  Serial.begin(115200);
+
+  while (!Serial)
+  ;
+
+  delay(1000);
+
   // Conecta ao Adafruit IO
   Serial.print("Conectando ao Adafruit IO");
   io.connect();
 
   // Registra função callback quando o feed receber dados
-  botaoLED->onMessage(handleBotaoLed);
+  
 
   // Aguarda conexão
   while(io.status() < AIO_CONNECTED) {
@@ -44,21 +68,25 @@ void setup() {
 }
 
 void loop() {
-  // Mantém conexão ativa
-  io.run();
-}
+//   // Mantém conexão ativa
+//   io.run();
+// }
 
-// Função chamada sempre que houver alteração no feed
-void handleBotaoLed(AdafruitIO_Data *data) {
-  String valor = data->toString();
-  Serial.print("Recebido do feed: ");
-  Serial.println(valor);
+// // Função chamada sempre que houver alteração no feed
+// void handleBotaoLed(AdafruitIO_Data *data) {
+//   String valor = data->toString();
+//   Serial.print("Recebido do feed: ");
+//   Serial.println(valor);
 
-  if (valor == "true") {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED LIGADO");
-  } else {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED DESLIGADO");
-  }
+//   if (valor == "true") {
+//     digitalWrite(LED_PIN, HIGH);
+//     Serial.println("LED LIGADO");
+//   } else {
+//     digitalWrite(LED_PIN, LOW);
+//     Serial.println("LED DESLIGADO");
+//   }
+
+  Serial.print(F("Distancia Lida: "));
+  Serial.println(sonar.ping_cm());
+  delay(500);
 }
